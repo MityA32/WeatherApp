6bf3be09a6a5936cf8e6837bb8ad67c178535acf
MityA32 <hetmandmitriy@gmail.com>
2023-09-04 16:56:03 +0300

    first commit

---
+++ WeatherApp/Modules/CustomNavigationBar/View/CustomNavigationNarView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomNavigationNarView.swift
+//  WeatherApp
+//
+//  Created by Dmytro Hetman on 04.09.2023.
+//
+
+import UIKit
+
+class CustomNavigationNarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
